694cc94bfc6a50ade393432ac3e9b73cb9d14abe
gms0417 <kwak_ms@naver.com>
2020-03-11 11:06:41 +0900

    action 추가

--- src/action/ActionForward.java
+++ src/action/ActionForward.java
@@ -1,5 +1,16 @@
 package action;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
+
 public class ActionForward {
 	private String path;
 	private boolean redirect;
