1d56fac1c9d6b6290508245b5b453adb1e6be011
gms0417 <kwak_ms@naver.com>
2020-03-11 11:08:12 +0900

    memberVO

--- src/domain/MemberVO.java
+++ src/domain/MemberVO.java
@@ -1,5 +1,19 @@
 package domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Setter
+@Getter
+@AllArgsConstructor
 public class MemberVO {
+	private String id;
+	private String password;
+	private String name;
+	private String addr;
+	
 
 }
